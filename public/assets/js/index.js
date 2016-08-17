76c4e74ef494196ff090e6976fe2a3854790d384
bunnieabc <bunnieabc@gmail.com>
2016-08-17 17:04:35 +0800

    finish web version

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -12,8 +12,7 @@ $(document).ready(function() {
   });
   //loading END
 
-  $(".number-20").animateNumbers(20, true, 2800);
-  $(".number-100").animateNumbers(100, true, 3800);
+ 
 
   $('.dark-cover').hover(function(){
     $(this).children().fadeIn(300);
@@ -29,8 +28,68 @@ $(document).ready(function() {
     $(this).css("opacity","1");
   })
   console.log($(".modal-block").height());
+  $(".cart").click(function(){
+    if (!$(".sidebar").hasClass("moveout")){
+      $(".sidebar").animate({
+        right: "0%",
+      }, 300);
+      $(".sidebar").addClass("moveout");
+    }
+    else{
+      $(".sidebar").animate({
+        right: "-33%",
+      }, 300);
+      $(".sidebar").removeClass("moveout");
+    }
+  });
 
+  $('.item-detail .item-detail-btn').click(function(){
+    // 當點到標題時，若答案是隱藏時則顯示它；反之則隱藏
+    $(this).next('.item-detail .item-detail-hide').slideToggle();
+    if($(this).children().hasClass("fa-caret-right"))
+      $(this).children().removeClass("fa-caret-right").addClass("fa-caret-down");
+    else
+      $(this).children().removeClass("fa-caret-down").addClass("fa-caret-right");
+  }).siblings('.item-detail .item-detail-hide').hide();
 
+  $(".detail-icon").click(function(){
+    if($(".modal-block").hasClass("move"))
+    {
+      $(".modal-block").css({left:"0%"},300);
+      $(".modal-block").removeClass("move");
+      $(".modal-block .modal-content-md .btn-cart").removeClass("btn-cart2")
+      $(".modal-block .modal-content-md .btn-cart")
+        .html("<span class='into'>進入購物車</span><span class='icon'><i class='fa fa-angle-right'></i></span>")
+    }
+  })
+
+  $(".cart-icon").click(function(){
+    if(!$(".modal-block").hasClass("move"))
+    {
+      $(".modal-block").css({left:"-59%"},300);
+      $(".modal-block").addClass("move");
+      //$(".modal-block .modal-content-md .btn-cart").removeClass("btn-cart2")
+      $(".modal-block .modal-content-md .btn-cart").addClass("btn-cart2")
+        .html("<span class='icon2'><i class='fa fa-angle-left'></i></span> <span class='back'>回到圖片</span><span class='space'></span> ")
+    }
+  })
+
+  $(".btn-cart").click(function(){
+    if(!$(".modal-block").hasClass("move"))
+    {
+      $(".modal-block").animate({left:"-59%"},300);
+      $(".modal-block").addClass("move");
+      $(".modal-block .modal-content-md .btn-cart").addClass("btn-cart2")
+        .html("<span class='icon2'><i class='fa fa-angle-left'></i></span> <span class='back'>回到圖片</span><span class='space'></span> ")
+    }
+    else{
+      $(".modal-block").animate({left:"0%"},300);
+      $(".modal-block").removeClass("move");
+      $(".modal-block .modal-content-md .btn-cart").removeClass("btn-cart2")
+      $(".modal-block .modal-content-md .btn-cart")
+        .html("<span class='into'>進入購物車</span><span class='icon'><i class='fa fa-angle-right'></i></span>")
+    }
+  })
 });
 
 //reveal效果
