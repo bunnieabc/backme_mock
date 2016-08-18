1c22c8b8a9100511d904cf1405f141983ad85158
bunnieabc <bunnieabc@gmail.com>
2016-08-18 13:55:17 +0800

    finish all mockup

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,24 +1,25 @@
 $(document).ready(function() {
   //loading
-  $(window).load(function() {
-    $("#loading").delay(600).fadeOut();
-  });
-  var progress = 0;
-  $("img").load(function() {
-    progress += 100 / $("img").length;
-    var percent = Math.round(progress) + "%";
-    $("#loading .txt").text(percent);
-    $("#loading .ld-bar").css("width", percent);
-  });
   //loading END
+  var w = window.innerWidth;
+  var h = window.innerHeight;
+  //console.log("test2");
 
- 
 
   $('.dark-cover').hover(function(){
-    $(this).children().fadeIn(300);
+    if(w>992){
+      $(this).children().fadeIn(300);
+    }
   }, function(){
-    $(this).children().fadeOut(300);
+    if(w>992){
+      $(this).children().fadeOut(300);
+    }
   })
+  if(w<992){
+    $(".item").attr("data-toggle","modal");
+    $(".item").attr("data-target","#myModal");
+
+  }
 
   $(".modal-img-sm-row .modal-img-sm").click(function(){
     $(this).attr('id');
@@ -27,22 +28,53 @@ $(document).ready(function() {
     $(".modal-img-sm-row .modal-img-sm").css("opacity","0.6");
     $(this).css("opacity","1");
   })
-  console.log($(".modal-block").height());
+  //sidebar part
   $(".cart").click(function(){
-    if (!$(".sidebar").hasClass("moveout")){
-      $(".sidebar").animate({
-        right: "0%",
-      }, 300);
-      $(".sidebar").addClass("moveout");
+    if(w>992){
+      if (!$(".sidebar").hasClass("moveout")){
+        $(".sidebar").animate({
+          right: "0%",
+        }, 300);
+        $(".sidebar").addClass("moveout");
+        $(".sidebar-dark-cover").fadeIn(300);
+      }
+      else{
+        $(".sidebar").animate({
+          right: "-33%",
+        }, 300);
+        $(".sidebar").removeClass("moveout");
+        $(".sidebar-dark-cover").fadeOut(300);
+      }
     }
     else{
+      if (!$(".sidebar").hasClass("moveout")){
+        $(".sidebar").animate({
+          right: "0%",
+        }, 300);
+        $(".sidebar").addClass("moveout");
+        $(".sidebar-dark-cover").fadeIn(300);
+      }
+      else{
+        $(".sidebar").animate({
+          right: "-95%",
+        }, 300);
+        $(".sidebar").removeClass("moveout");
+        $(".sidebar-dark-cover").fadeOut(300);
+      }
+    }
+  });
+
+  $(".sidebar-dark-cover").click(function(){
+    if ($(".sidebar").hasClass("moveout")){
       $(".sidebar").animate({
         right: "-33%",
       }, 300);
       $(".sidebar").removeClass("moveout");
+      $(".sidebar-dark-cover").fadeOut(300);
     }
   });
 
+  //toggle
   $('.item-detail .item-detail-btn').click(function(){
     // 當點到標題時，若答案是隱藏時則顯示它；反之則隱藏
     $(this).next('.item-detail .item-detail-hide').slideToggle();
@@ -52,44 +84,84 @@ $(document).ready(function() {
       $(this).children().removeClass("fa-caret-down").addClass("fa-caret-right");
   }).siblings('.item-detail .item-detail-hide').hide();
 
+  //modal part
   $(".detail-icon").click(function(){
+    var str ="<span class='back hidden-lg hidden-md' data-dismiss='modal'>回到商場</span><span class='transform-btn'><span class='into'>進入購物車</span><span class='icon'><i class='fa fa-angle-right'></i></span></span>"
     if($(".modal-block").hasClass("move"))
     {
       $(".modal-block").css({left:"0%"},300);
       $(".modal-block").removeClass("move");
       $(".modal-block .modal-content-md .btn-cart").removeClass("btn-cart2")
-      $(".modal-block .modal-content-md .btn-cart")
-        .html("<span class='into'>進入購物車</span><span class='icon'><i class='fa fa-angle-right'></i></span>")
+      $(".modal-block .modal-content-md .btn-cart").html(str);
     }
   })
 
   $(".cart-icon").click(function(){
+    var str = "<span class='transform-btn'><span class='icon2'><i class='fa fa-angle-left'></i></span><span class='back'>回到圖片</span></span><span class='space'></span> ";
     if(!$(".modal-block").hasClass("move"))
     {
       $(".modal-block").css({left:"-59%"},300);
       $(".modal-block").addClass("move");
       //$(".modal-block .modal-content-md .btn-cart").removeClass("btn-cart2")
-      $(".modal-block .modal-content-md .btn-cart").addClass("btn-cart2")
-        .html("<span class='icon2'><i class='fa fa-angle-left'></i></span> <span class='back'>回到圖片</span><span class='space'></span> ")
+      $(".modal-block .modal-content-md .btn-cart").addClass("btn-cart2").html(str);
     }
   })
 
-  $(".btn-cart").click(function(){
-    if(!$(".modal-block").hasClass("move"))
+  $(document).on("click", ".transform-btn", function(){
+    //console.log("I dont know why");
+    if(w>992)
     {
-      $(".modal-block").animate({left:"-59%"},300);
-      $(".modal-block").addClass("move");
-      $(".modal-block .modal-content-md .btn-cart").addClass("btn-cart2")
-        .html("<span class='icon2'><i class='fa fa-angle-left'></i></span> <span class='back'>回到圖片</span><span class='space'></span> ")
+      if(!$(".modal-block").hasClass("move"))
+      {
+        var str = "<span class='transform-btn'><span class='icon2'><i class='fa fa-angle-left'></i></span><span class='back'>回到圖片</span></span><span class='space'></span> "
+        $(".modal-block").animate({left:"-59%"},300);
+        $(".modal-block").addClass("move");
+        $(".modal-block .modal-content-md .btn-cart").addClass("btn-cart2")
+          .html(str)
+      }
+      else{
+        //alert("2");
+        var str ="<span class='back hidden-lg hidden-md' data-dismiss='modal'>回到商場</span><span class='transform-btn'><span class='into'>進入購物車</span><span class='icon'><i class='fa fa-angle-right'></i></span></span>"
+        $(".modal-block").animate({left:"0%"},300);
+        $(".modal-block").removeClass("move");
+        $(".modal-block .modal-content-md .btn-cart").removeClass("btn-cart2")
+        $(".modal-block .modal-content-md .btn-cart").html(str)
+      }
     }
-    else{
-      $(".modal-block").animate({left:"0%"},300);
-      $(".modal-block").removeClass("move");
-      $(".modal-block .modal-content-md .btn-cart").removeClass("btn-cart2")
-      $(".modal-block .modal-content-md .btn-cart")
-        .html("<span class='into'>進入購物車</span><span class='icon'><i class='fa fa-angle-right'></i></span>")
+    else
+    {
+      var str = "<span class='transform-btn'><span class='icon2'><i class='fa fa-angle-left'></i></span><span class='back'>回到圖片</span></span><span class='space'></span> "
+      if(!$(".modal-block").hasClass("move"))
+      {
+        $(".modal-block").animate({left:"-100%"},300);
+        $(".modal-block").addClass("move");
+        $(".modal-block .modal-content-md .btn-cart").addClass("btn-cart2")
+          .html(str)
+      }
+      else{
+        var str ="<span class='back hidden-lg hidden-md' data-dismiss='modal'>回到商場</span><span class='transform-btn'><span class='into'>進入購物車</span><span class='icon'><i class='fa fa-angle-right'></i></span></span>"
+        $(".modal-block").animate({left:"0%"},300);
+        $(".modal-block").removeClass("move");
+        $(".modal-block .modal-content-md .btn-cart").removeClass("btn-cart2")
+        $(".modal-block .modal-content-md .btn-cart").html(str)
+      }
     }
   })
+
+  $(".goto").click(function(){
+    
+    //$(".num").addClass("num-animation"); 
+    
+      setTimeout(function(){
+        $(".num").addClass("num-animation"); 
+        setTimeout(function(){
+          $(".num").removeClass("num-animation");
+        },400)  
+      },200);
+    
+    
+  })
+
 });
 
 //reveal效果
